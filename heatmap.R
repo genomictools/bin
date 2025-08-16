cb4c7cc12181729e704b987f4a2ceddc7f37ed91
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-08-16 21:48:05 +0100

    use cnvr

--- heatmap.R
+++ heatmap.R
@@ -10,55 +10,16 @@ type      <- args[4]
 n_samples <- args[5]
 n_genes   <- args[6]
 
-# cnv   <- 'call-cnv-arrays/tests/results/scanned/pheno2.gene.cnv'
-# density <- 0
-# output  <- 'test.png'
-
 # load cnv
-d <- readr::read_table(cnv, col_names = FALSE)
-d <- dplyr::select(d, region = X1, cn = X4, gene = X9, file = X5)
-d <- dplyr::filter(d, gene != 'NOT_FOUND')
-d <- dplyr::mutate(d, cn = as.integer(stringr::str_remove_all(cn, '.*=')))
-d <- transform(d, gene = strsplit(gene, ','))
-d <- tidyr::unnest(d, gene)
-d <- tidyr::separate(d, file, c('cohort', 'key'), remove = FALSE)
-d <- tidyr::separate(d, region, c('chrom', 'start', 'end'), remove = FALSE)
+col_names <- c('region', 'numsnp', 'length', 'cn', 'sample', 'startsnp', 'endsnp', 'conf', 'gene', 'exon')
+cnv <- cnvr::read_cnv(cnv, col_names)
 
-# columns: sample_count
-sample_count <- table(d$file)
-ca <- ComplexHeatmap::columnAnnotation(
-  '# Samples' = ComplexHeatmap::anno_barplot(as.integer(sample_count))
-)
-
-# rows: gene_count
-gene_count <- table(d$gene)
-ra <- ComplexHeatmap::rowAnnotation(
-  '# Variants' = ComplexHeatmap::anno_barplot(as.integer(gene_count))
-)
-
-# heatmap: cnvs
-heat_matrix <- reshape2::acast(
-  d,
-  gene ~ key,
-  value.var = 'cn',
-  fill = '2',
-  fun.aggregate = function(x) paste(unique(x), collapse = ',')
-)
-
-colors <- c("darkblue", "blue", "white", "red", "darkred") 
-names(colors) <- c('0', '1', '2', '3', '4')
+samples <- stringr::str_split(cnv$sample, '\\.', simplify = TRUE)[, 2]
 
 file_name <- paste(cohort, feature, type, 'png', sep = '.')
 
 png(filename = file_name)
 
-ComplexHeatmap::Heatmap(
-  heat_matrix,
-  col = colors,
-  left_annotation = ra,
-  top_annotation = ca,
-  name = 'CNV',
-  rect_gp = grid::gpar(col = "white", lwd = 2)
-)
+cnvr::plot_heatmap(cnv, column_labels = sort(unique(samples)))
 
 dev.off()
--- scatter.R
+++ scatter.R
@@ -10,103 +10,62 @@ type    <- args[4]
 flank   <- args[5]
 top_n   <- args[6]
 
-# cnv   <- 'call-cnv-arrays/tests/results/scanned/pheno2.gene.cnv'
-# signal<- 'pheno1.sample1.data.txt.adjusted,pheno1.sample3.data.txt.adjusted,pheno2.sample5.data.txt.adjusted,pheno1.sample2.data.txt.adjusted,pheno2.sample4.data.txt.adjusted'
-# pfb   <- 'call-cnv-arrays/tests/results/ref/common_all.pfb'
-# type <- 'LRR'
-# flank <- 1000
-# top_n <- 1
-
 # load cnv
-d <- readr::read_table(cnv, col_names = FALSE)
-d <- dplyr::select(d, region = X1, gene = X9, file = X5)
-d <- dplyr::filter(d, gene != 'NOT_FOUND')
-d <- transform(d, gene = strsplit(gene, ','))
-d <- tidyr::unnest(d, gene)
-d <- tidyr::separate(d, file, c('cohort', 'key'), remove = FALSE)
-d <- tidyr::separate(d, region, c('chrom', 'start', 'end'), remove = FALSE)
-d <- dplyr::group_by(d, gene)
-d <- dplyr::reframe(
-  d,
-  n_variants = length(unique(region)),
-  n_samples = length(unique(key)),
-  chrom = unique(chrom),
-  start = min(as.integer(start)),
-  end = max(as.integer(end)),
-  cohort = list(unique(cohort)),
-  key = list(unique(key)),
-  file = list(unique(file))
-)
+col_names <- c('region', 'numsnp', 'length', 'cn', 'sample', 'startsnp', 'endsnp', 'conf', 'gene', 'exon')
+cnv <- cnvr::read_cnv(cnv, col_names)
+cnv <- cnv[cnv$gene != 'NOT_FOUND']
+cnv <- split(cnv, cnv$region)
 
-d <- dplyr::top_n(d, top_n, n_samples)
-
-grl <- GenomicRanges::makeGRangesListFromDataFrame(d, split.field = 'gene', keep.extra.columns = TRUE)
-# grl <- purrr::map(grl, GenomicRanges::promoters)
-
-# load marker info
-pfb <- readr::read_tsv(
-  pfb,
-  skip = 1,
-  col_select = 1:3,
-  col_names = c('Name', 'chrom', 'pos')
-)
+# load pfb
+pfb <- cnvr::read_pfb(pfb, c('name', 'chr', 'pos', 'pfb'))
 
 # load signal
+col_names <- c('name', 'baf', 'lrr')
 signal <- unlist(strsplit(signal, ','))
 names(signal) <- signal
 
-signal <- purrr::map_df(signal, readr::read_tsv, .id = 'file')
-signal <- dplyr::inner_join(signal, pfb)
-signal <- dplyr::mutate(signal, chrom = paste0('chr', chrom))
-signal <- GenomicRanges::makeGRangesFromDataFrame(
+signal <- purrr::imap(
   signal,
-  start.field = 'pos',
-  end.field = 'pos', 
-  keep.extra.columns = TRUE
+  ~{
+    gr <- cnvr::read_signal(.x, col_names = col_names, pfb = pfb)
+    gr$sample <- .y
+    gr
+  }
 )
-signal <- GenomicRanges::sort(signal)
+signal <- GenomicRanges::GRangesList(signal)
+signal <- unlist(signal)
 
-# plot
-purrr::imap(grl, function(x, .y) {
-  gene_name <- .y
-  
-  sub_signal <- signal[signal$file %in% unlist(x$file)]
-  
-  ol <- IRanges::subsetByOverlaps(sub_signal, x)
-  oll <- split(ol, ol$file)
+# load dbs
+txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene
+org <- org.Hs.eg.db::org.Hs.eg.db
+keys <- AnnotationDbi::keys(org, 'SYMBOL')
 
-  purrr::imap(oll, function(x, .y) {
-    cohort <- stringr::str_split(.y, '\\.', simplify = TRUE)[1]
-    sample <- stringr::str_split(.y, '\\.', simplify = TRUE)[2]
+purrr::imap(
+  cnv,
+  ~{
+    # get gene model
+    gene <- unlist(strsplit(.x$gene, ','))
+    gene <- head(intersect(gene, keys), 2)
+    gene_models <- cnvr::get_genemodel(txdb, org, gene)
     
-    file_name <- paste(cohort, sample, gene_name, type, 'png', sep = '.')
+    # get overlap
+    sub_signal <- signal[signal$sample == .x$sample]
+    ol <- cnvr::get_overlap(
+      .x,
+      sub_signal,
+      flank = GenomicRanges::width(.x)/2
+    )
     
+    # make plot
+    cohort <- unlist(strsplit(.x$sample, '\\.'))[1]
+    sample <- unlist(strsplit(.x$sample, '\\.'))[2]
+    file_name <- paste(cohort, sample, .x$region, type, 'png', sep = '.')
     png(filename = file_name, width = 4, height = 4, units = 'in', res = 300)
-    
-    if ( type == 'lrr' ) {
-      plot(
-        IRanges::start(x),
-        x$`Log R Ratio`,
-        ylim = c(-2, 2),
-        pch = 19,
-        cex = .7,
-        xlab = unique(GenomicRanges::seqnames(x)),
-        ylab = 'LRR',
-        main = paste0(gene_name, " (", cohort, ".", sample, ")")
-      )
-    } else if ( type == 'baf' ) {
-      plot(
-        IRanges::start(x),
-        x$`B Allele Freq`,
-        ylim = c(0, 1),
-        pch = 19,
-        cex = .7,
-        xlab = unique(GenomicRanges::seqnames(x)),
-        ylab = 'BAF',
-        main = paste0(gene_name, " (", cohort, ".", sample, ")")
-      )
-    }
+    cnvr::plot_signal(
+      ol,
+      type = toupper(type), ylab = toupper(type),
+      gene_model = gene_models
+    )
     dev.off()
-  
-  })
-})
+  }
+)

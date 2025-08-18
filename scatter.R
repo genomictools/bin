d7b0501531210472131e842529b06f9566409fee
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-08-18 21:15:31 +0100

    split by sample, read, and plot

--- scatter.R
+++ scatter.R
@@ -14,26 +14,10 @@ top_n   <- args[6]
 col_names <- c('region', 'numsnp', 'length', 'cn', 'sample', 'startsnp', 'endsnp', 'conf', 'gene', 'exon')
 cnv <- cnvr::read_cnv(cnv, col_names)
 cnv <- cnv[cnv$gene != 'NOT_FOUND']
-cnv <- split(cnv, cnv$region)
+cnv <- split(cnv, cnv$sample)
 
 # load pfb
 pfb <- cnvr::read_pfb(pfb, c('name', 'chr', 'pos', 'pfb'))
-
-# load signal
-col_names <- c('name', 'baf', 'lrr')
-signal <- unlist(strsplit(signal, ','))
-names(signal) <- signal
-
-signal <- purrr::imap(
-  signal,
-  ~{
-    gr <- cnvr::read_signal(.x, col_names = col_names, pfb = pfb)
-    gr$sample <- .y
-    gr
-  }
-)
-signal <- GenomicRanges::GRangesList(signal)
-signal <- unlist(signal)
 
 # load dbs
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene
@@ -48,31 +32,44 @@ keys <- AnnotationDbi::select(org, keys, 'SYMBOL', 'ENTREZID')$SYMBOL
 purrr::imap(
   cnv,
   ~{
-    # get gene model
-    gene <- unlist(strsplit(.x$gene, ','))
-    if ( length(gene) ) {
-      gene <- intersect(gene, keys)
-      gene_models <- cnvr::get_genemodel(txdb, org, gene)
+    # load signal
+    signal <- cnvr::read_signal(
+      unique(.x$sample),
+      col_names = c('name', 'baf', 'lrr'),
+      pfb = pfb
+    )
 
-      # get overlap
-      sub_signal <- signal[signal$sample == .x$sample]
-      ol <- cnvr::get_overlap(
-        .x,
-        sub_signal,
-        flank = GenomicRanges::width(.x)/2
-      )
+    # split by region
+    gr <- split(.x, .x$region)
+    purrr::imap(
+      gr,
+      ~{
+        # get gene model
+        gene <- unlist(strsplit(.x$gene, ','))
+        if ( length(gene) ) {
+          gene <- intersect(gene, keys)
+          gene_models <- cnvr::get_genemodel(txdb, org, gene)
 
-      # make plot
-      cohort <- unlist(strsplit(.x$sample, '\\.'))[1]
-      sample <- unlist(strsplit(.x$sample, '\\.'))[2]
-      file_name <- paste(cohort, sample, .x$region, type, 'png', sep = '.')
-      png(filename = file_name, width = 4, height = 4, units = 'in', res = 300)
-      cnvr::plot_signal(
-        ol,
-        type = toupper(type), ylab = toupper(type),
-        gene_model = gene_models
-      )
-      dev.off()
-    }
+          # get overlap
+          ol <- cnvr::get_overlap(
+            .x,
+            signal,
+            flank = GenomicRanges::width(.x)/2
+          )
+
+          # make plot
+          cohort <- unlist(strsplit(.x$sample, '\\.'))[1]
+          sample <- unlist(strsplit(.x$sample, '\\.'))[2]
+          file_name <- paste(cohort, sample, .x$region, type, 'png', sep = '.')
+          png(filename = file_name, width = 5, height = 4 + length(gene) / 2, units = 'in', res = 300)
+          cnvr::plot_signal(
+            ol,
+            type = toupper(type), ylab = toupper(type),
+            gene_model = gene_models
+          )
+          dev.off()
+        }
+      }
+    )
   }
 )

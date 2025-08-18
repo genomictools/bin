77e8fe5ded79cf4dc4d3873e5c6e0d30ff467726
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-08-18 15:51:47 +0100

    add n params to heatmpa

--- heatmap.R
+++ heatmap.R
@@ -14,12 +14,14 @@ n_genes   <- args[6]
 col_names <- c('region', 'numsnp', 'length', 'cn', 'sample', 'startsnp', 'endsnp', 'conf', 'gene', 'exon')
 cnv <- cnvr::read_cnv(cnv, col_names)
 
-samples <- stringr::str_split(cnv$sample, '\\.', simplify = TRUE)[, 2]
-
 file_name <- paste(cohort, feature, type, 'png', sep = '.')
 
 png(filename = file_name)
 
-cnvr::plot_heatmap(cnv, column_labels = sort(unique(samples)))
+cnvr::plot_heatmap(
+    cnv,
+    top_samples = as.integer(n_samples),
+    top_genes = as.integer(n_genes)
+)
 
 dev.off()

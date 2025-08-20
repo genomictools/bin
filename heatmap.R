f82aeb92e4ae68d2fd9ca76ff6ab16a9558c35ea
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-08-20 11:46:26 +0100

    subset by gene list

--- heatmap.R
+++ heatmap.R
@@ -7,12 +7,23 @@ cohort    <- args[1]
 feature   <- args[2]
 cnv       <- args[3]
 type      <- args[4]
-n_samples <- args[5]
-n_genes   <- args[6]
+genelist  <- args[5]
+n_samples <- args[6]
+n_genes   <- args[7]
 
 # load cnv
 col_names <- c('region', 'numsnp', 'length', 'cn', 'sample', 'startsnp', 'endsnp', 'conf', 'gene', 'distance')
 cnv <- cnvr::read_cnv(cnv, col_names)
+
+if (length(genelist) > 1) {
+  ind <- purrr::map2_lgl(
+    strsplit(cnv$gene, ','),
+    strsplit(genelist, ','),
+    ~{sum(.x %in% .y) > 0}
+  )
+  
+  cnv <- cnv[ind]
+}
 
 file_name <- paste(cohort, feature, type, 'heatmap', 'png', sep = '.')
 

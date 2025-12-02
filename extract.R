f60f76eda46cbfd41d4aac6c2ada27d4f7bd5fff
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-12-02 23:51:01 +0000

    extract in r

---
+++ extract.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+
+# Capture command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+file     <- args[1]
+name_col <- args[2]
+baf_col  <- args[3]
+lrr_col  <- args[4]
+a1_col   <- args[5]
+a2_col   <- args[6]
+output   <- args[7]
+
+# Get number of lines to skip
+skip <- grep("\\[Data\\]", readr::read_lines(file, n_max = 100))
+skip <- ifelse(length(skip) == 0, 0, skip)
+
+# Read input file
+res <- readr::read_tsv(file, skip = skip)
+
+# Create GType column
+if (a1_col != 'null' & a2_col != 'null') {
+  res <- tidyr::unite(
+    res,
+    'GType',
+    dplyr::any_of(c(a1_col, a2_col)),
+    sep = ''
+  )
+}
+
+# Subset columns
+res <- dplyr::select(
+  res,
+  dplyr::any_of(c(name_col, baf_col, lrr_col, 'GType'))
+)
+
+# Rename columns
+col_names <- c("Name", "B Allele Freq", "Log R Ratio")
+if (ncol(res) > 3) { col_names <- c(col_names, 'GType') }
+res <- setNames(res, col_names)
+
+# Write to file
+readr::write_tsv(res, output)
